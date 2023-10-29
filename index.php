33bd2c9dbd3cbf860e4f56fef891a4c01fbafc98
Amir Bhatti <amirbhatti045@gmail.com>
2023-10-29 07:07:56 -0700

    hello
    hello

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Tempo Bootstrap Template - Index</title>
+
+  <?php include"includes/files.html"; ?>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include"includes/header.php"; ?>
+
+  <!-- ======= Hero Section ======= -->
+  <?php include"includes/herosection.php"; ?>
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <?php include"includes/aboutsection.php"; ?>~~
+
+    <!-- ======= Services Section ======= -->
+    <?php include"includes/servicesection.php"; ?>
+
+    <!-- ======= Features Section ======= -->
+    <?php include"includes/features.php"; ?>
+
+    <!-- ======= Cta Section ======= -->
+    <?php include"includes/calltoaction.php"; ?>
+
+    <!-- ======= Portfolio Section ======= -->
+    <?php include"includes/portfolio.php"; ?>
+
+    <!-- ======= Pricing Section ======= -->
+    <?php include"includes/pricing.php"; ?>
+
+    <!-- ======= F.A.Q Section ======= -->
+    <?php include"includes/faqsection.php"; ?>
+
+    <!-- ======= Team Section ======= -->
+    <?php include"includes/team.php"; ?>
+
+    <!-- ======= Contact Section ======= -->
+    <?php include"includes/contectus.php"; ?>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include"includes/footer.php"; ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
